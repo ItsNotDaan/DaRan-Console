1bab16508bd2210505284188aa640b1242bc396c
ItsNotDaan <Daanheetkamp@hotmail.com>
2021-02-10 14:11:57 +0100

    xa

--- main.ino
+++ main.ino
@@ -14,11 +14,29 @@
 
 // ----- Declare Constants -----
 
+//IO
+#define LEDARRAY_D 2
+#define LEDARRAY_C 3
+#define LEDARRAY_B 4
+#define LEDARRAY_A 5
+#define LEDARRAY_G 6
+#define LEDARRAY_DI 7
+#define LEDARRAY_CLK 8
+#define LEDARRAY_LAT 9
+
+#define	linksKnop 8
+#define rechtsKnop 9
+#define	middenKnop 10
+
 
 // ----- Declare Objects -----
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // ----- Declare subroutines and/or functions -----
+unsigned char Display_Buffer[2];
+
+unsigned int Word1[32];
+int knop;
 
 // ----- Declare Global Variables -----
 
@@ -27,16 +45,229 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // Setup
 void setup()
 {
+  pinMode(LEDARRAY_D, OUTPUT);
+	pinMode(LEDARRAY_C, OUTPUT);
+	pinMode(LEDARRAY_B, OUTPUT);
+	pinMode(LEDARRAY_A, OUTPUT);
+	pinMode(LEDARRAY_G, OUTPUT);
+	pinMode(LEDARRAY_DI, OUTPUT);
+	pinMode(LEDARRAY_CLK, OUTPUT);
+	pinMode(LEDARRAY_LAT, OUTPUT);
+	pinMode(linksKnop, INPUT);
+	pinMode(rechtsKnop, INPUT);
+	pinMode(middenKnop, INPUT);
+
+  Serial.begin(9600);
   lcd.begin(16,2);
-  lcd.setCursor(3,0);
-  lcd.print("Mini Gamble");
-  
+  lcd.setCursor(0,0);
+
+
+
+	//here you'll draw the scene in your screen
+	unsigned char  Scene[16][16] =
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
+
 }
 
 // Main loop
 void loop()
 {
+ update();
+ Display(Word1);
+
+	if (rechtsKnop == true)
+	{
+		knop = 1
+	}
+	else if (linksKnop == true)
+	{
+		knop = 2
+	}
+	else if (middenKnop == true)
+	{
+		knop = 3
+	}
+
+
+	switch (knop)
+	{
+		case 1:
+		{
+			lcd.display("Rechterknop");
+
+		}
+
+		case 2:
+		{
+			lcd.display("Linkerknop");
+		}
+
+		case 3:
+		{
+			lcd.display("Selecteer Knop");
+		}
+	}
+}
 
 
 
+
+
+
+
+
+void update(){
+	clearWord();
+	SceneToWord();
+}
+
+void clearWord(){
+	for(int i = 0; i < 32; i++)
+		Word1[i] = 0;
+}
+
+void SceneToWord(){
+	int i, k, key = 0;
+	unsigned int value;
+	for(i = 0; i < 16; i++){
+		for(k = 0; k < 16; k++){
+			if(i < 8){
+				value = Scene[i][k] << (7 - i);
+				Word1[15 - k] += value;
+			} else {
+				value = Scene[i][k] << (15 - i);
+				Word1[31 - k] += value;
+			}
+		}
+	}
+}
+
+
+void Display(unsigned int dat[])	{
+	unsigned char i;
+
+	for( i = 0 ; i < 16 ; i++ )
+	{
+		digitalWrite(LEDARRAY_G, HIGH);
+
+		Display_Buffer[0] = dat[i];
+		Display_Buffer[1] = dat[i+16];
+
+		Send(Display_Buffer[1]);
+		Send(Display_Buffer[0]);
+
+		digitalWrite(LEDARRAY_LAT, HIGH);
+		delayMicroseconds(1);
+
+		digitalWrite(LEDARRAY_LAT, LOW);
+		delayMicroseconds(1);
+
+		Scan_Line(i);
+
+		digitalWrite(LEDARRAY_G, LOW);
+
+		delayMicroseconds(100);
+	}
+}
+
+
+void Scan_Line( unsigned int m) {
+	switch(m)
+	{
+		case 0:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 1:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 2:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 3:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 4:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 5:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 6:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 7:
+			digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 8:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 9:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 10:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 11:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 12:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 13:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		case 14:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);
+			break;
+		case 15:
+			digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);
+			break;
+		default : break;
+	}
+}
+
+
+void Send( unsigned int dat) {
+	unsigned char i;
+	digitalWrite(LEDARRAY_CLK, LOW);
+	delayMicroseconds(1);;
+	digitalWrite(LEDARRAY_LAT, LOW);
+	delayMicroseconds(1);;
+
+	for( i = 0 ; i < 8 ; i++ )
+	{
+		if( dat&0x01 )
+		{
+			digitalWrite(LEDARRAY_DI, HIGH);
+		}
+		else
+		{
+			digitalWrite(LEDARRAY_DI, LOW);
+		}
+
+		delayMicroseconds(1);
+		digitalWrite(LEDARRAY_CLK, HIGH);
+			delayMicroseconds(1);
+		digitalWrite(LEDARRAY_CLK, LOW);
+			delayMicroseconds(1);
+		dat >>= 1;
+
+	}
 }
