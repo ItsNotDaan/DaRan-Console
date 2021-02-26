31e372add1129dbfb3b1dc77c568513ad125f79c
ItsNotDaan <daanheetkamp@hotmail.com>
2021-02-26 10:53:47 +0100

    tranciever 2

--- nRF24L01/DaRan-Ontvanger.ino
+++ nRF24L01/DaRan-Ontvanger.ino
@@ -1,35 +1,54 @@
-//Include Libraries
+/*
+  Name: DaRan Ontvanger
+  Date:
+  Author: Daan Heetkamp
+
+  Description:
+  Zorg dat er met deze code data kan worden ontvangen.
+
+  Revision:
+  0.1
+
+*/
+
+// ----- include libraries -----
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(9, 8);  // CE, CSN
+// ----- Declare Constants -----
+const byte address[6] = "00001"; //Dit is het adress waar naar gekeken wordt.
 
-//address through which two modules communicate.
-const byte address[6] = "00001";
+// ----- Declare Objects -----
+RF24 radio(9, 8);  // CE, CSN. Dit is nodig voor de librarie om te kijken welke pin de ontvanger is aangesloten.
 
+// ----- Declare subroutines and/or functions -----
+
+// ----- Declare Global Variables -----
+
+
+// Setup
 void setup()
 {
-  while (!Serial);
-    Serial.begin(9600);
+  Serial.begin(9600); //Start een seriele verbinding.
 
-  radio.begin();
+  radio.begin(); //zorg dat de radio begint met luisteren
 
   //set the address
-  radio.openReadingPipe(0, address);
+  radio.openReadingPipe(0, address); //adres dat ook in de constant werd aangegeven.
 
   //Set module as receiver
-  radio.startListening();
+  radio.startListening(); //dit is een ontvanger. .startSending is een zender.
 }
 
+// Main loop
 void loop()
 {
   //Read the data if available in buffer
-  if (radio.available())
+  if (radio.available()) //als er iets binnenkomt.
   {
-    char text[32] = {0};
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    char text[32] = {0}; //maak een array genaamd tekst waar 32 chars in kunnen. de {0} is om hem leeg te hebben. Er zijn dus 32 karakters met een 0.
+    radio.read(&text, sizeof(text)); //alles dat wordt ingelezen wordt opgeslagen in de char text.
+    Serial.println(text); //text wordt gescgrevcen in de seriele monitor.
   }
 }
--- nRF24L01/DaRan-Zender.ino
+++ nRF24L01/DaRan-Zender.ino
@@ -1,29 +1,48 @@
-//Include Libraries
+/*
+  Name: DaRan Zender
+  Date:
+  Author: Daan Heetkamp
+
+  Description:
+  Met deze code kan er verzonden worden met de zender.
+
+  Revision:
+  0.1
+
+*/
+
+// ----- include libraries -----
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(9, 8);  // CE, CSN
+// ----- Declare Constants -----
+const byte address[6] = "00001"; //Dit is het adress waar naar verzonden wordt.
 
-//address through which two modules communicate.
-const byte address[6] = "00001";
+// ----- Declare Objects -----
+RF24 radio(9, 8);  // CE, CSN Dit is nodig voor de library om te kijken welke pinnen de zender is aangesloten.
 
+// ----- Declare subroutines and/or functions -----
+
+// ----- Declare Global Variables -----
+
+
+// Setup
 void setup()
 {
-  radio.begin();
+  radio.begin(); //start de zender
 
-  //set the address
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(address); //init het adres van de zender.
 
-  //Set module as transmitter
-  radio.stopListening();
+  radio.stopListening(); //door te stoppen met luisteren wordt het een zender.
 }
+
+// Main loop
 void loop()
 {
   //Send message to receiver
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+  const char text[] = "Hello World"; //maak een array met karakters genaamd text. Stop hierin "Hello World".
+  radio.write(&text, sizeof(text)); //verstuur de data in de text.
 
-  delay(1000);
+  delay(1000); //wachtb 1 seconden met het verzenden van het bericht.
 }
