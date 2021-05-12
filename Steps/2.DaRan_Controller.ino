76110dc75fe3c2e863f7187791f10337048a87b3
ItsNotDaan <Daanheetkamp@hotmail.com>
2021-05-12 14:47:09 +0200

    Working Software!

--- Steps/2.DaRan_Controller.ino
+++ Steps/2.DaRan_Controller.ino
@@ -134,7 +134,6 @@ void loop()
       //while(digitalRead(knop) == LOW);
       isGedrukt = false; //The next game the button will be able to be pressed again. Its a reset.
     }
-<<<<<<< HEAD
 
     else if (text == '2') //Start game 2 Not important now.
     {
@@ -171,8 +170,6 @@ void loop()
       tone(buzz, 1500);
     }
 
-=======
->>>>>>> ed4c6ff994c8390f3ccf6b25ff997d59a8f96951
     ///delay(1000); //For testing purposes.
     noTone(buzz); //Make sure the buzzer is out.
   }
@@ -180,9 +177,7 @@ void loop()
 
 void sendMessage(t_message &msg)
 {
-  radio.stopListening(); //Start writing.
   radio.write(&msg, sizeof(t_message));
-  radio.startListening();
 }
 
 bool getMessage(t_message &msg)
