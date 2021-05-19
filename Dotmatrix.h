b53be5f9d7e0d8a5ecdc344d6686e937ad3f3c2c
Emran19 <78594299+Emran19@users.noreply.github.com>
2021-05-19 09:24:49 +0200

    Add files via upload

---
+++ Dotmatrix.h
@@ -0,0 +1,27 @@
+#ifndef Dotmatrix_h
+#define Dotmatrix_h
+
+#include <SPI.h>
+#include <Arduino.h>
+#define Num_Of_Word 4
+
+const unsigned char (One[Num_Of_Word][32]);
+
+const unsigned char (Two[Num_Of_Word][32]);
+
+const unsigned char (Three[Num_Of_Word][32]);
+
+const unsigned char (DiceOne[Num_Of_Word][32]);
+
+const unsigned char (DiceTwo[Num_Of_Word][32]);
+
+const unsigned char (DiceThree[Num_Of_Word][32]);
+
+const unsigned char (DiceFour[Num_Of_Word][32]);
+
+const unsigned char (DiceFive[Num_Of_Word][32]);
+
+const unsigned char (DiceSix[Num_Of_Word][32]);
+
+
+#endif
